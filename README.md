cbb2dc1ab3bd6972b5f687c90d75ea5496b2f55e
Adrian Pucułek <puculeka@gmail.com>
2021-03-07 21:40:58 +0100

    New sample and README update

--- README.md
+++ README.md
@@ -17,13 +17,16 @@ fun composableFun() {
             // update items and set isRefreshing = false
         }
     ) {
-        // Any composable content can go here
+        // Any scrollable composable content can go here
         LazyColumn {
             items.map { Text("I'm $it") }
         }
     }
 }
 ```
+
+You can put any composable in closure as long, as it is scrollable. If you are using `LazyColumn`, it is scrollable by default. With `Column` or `Box` use `Modifier.verticalScroll(rememberScrollState())` modifier.
+
 
 You can find more in [sample](https://github.com/poculeka/PullToRefresh/blob/master/pulltorefresh/app/src/main/java/com/puculek/pulltorefresh/samples/PullToRefreshSample.kt)
 
--- pulltorefresh/app/src/main/java/com/puculek/pulltorefresh/samples/PullToRefreshSample.kt
+++ pulltorefresh/app/src/main/java/com/puculek/pulltorefresh/samples/PullToRefreshSample.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -17,7 +19,7 @@ import kotlinx.coroutines.launch
 
 @Preview
 @Composable
-fun PullToRefreshPreview() {
+fun PullToRefreshWithLazyColumn() {
     var isRefreshing: Boolean by remember { mutableStateOf(false) }
     val scope = rememberCoroutineScope()
     var elements by remember { mutableStateOf(listOf(5, 4, 3, 2, 1)) }
@@ -35,23 +37,55 @@ fun PullToRefreshPreview() {
     ) {
         LazyColumn {
             elements.map {
-                item {
-                    Column(
-                        modifier = Modifier
-                            .padding(24.dp)
-                            .background(Color.Yellow)
-                            .padding(16.dp)
-                    ) {
-                        Text("$it", fontSize = 24.sp)
-                        Text(
-                            text = """
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            ssed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-                        """.trimIndent()
-                        )
-                    }
-                }
+                item { Item(it.toString()) }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun PullToRefreshWithColumn() {
+    var isRefreshing: Boolean by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    var elements by remember { mutableStateOf(listOf(5, 4, 3, 2, 1)) }
+    PullToRefresh(
+        isRefreshing = isRefreshing,
+        onRefresh = {
+            isRefreshing = true
+            scope.launch {
+                delay(1000)
+                elements = listOf(elements.size + 1) + elements
+                isRefreshing = false
+            }
+
+        }
+    ) {
+        Column(
+            modifier = Modifier.verticalScroll(rememberScrollState())
+        ) {
+            elements.map {
+                Item(it.toString())
+            }
+        }
+    }
+}
+
+@Composable
+fun Item(text: String) {
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .background(Color.Yellow)
+            .padding(16.dp)
+    ) {
+        Text(text, fontSize = 24.sp)
+        Text(
+            text = """
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+               ssed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+               """.trimIndent()
+        )
+    }
+
 }
